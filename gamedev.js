906bc5a50affccefa56cd681ba1f812bbd6abffb
KnightDips <210429@hkis.edu.hk>
2016-05-19 09:36:21 +0800

    MOVING WORK

--- gamedev.js
+++ gamedev.js
@@ -1,5 +1,23 @@
 $( document ).ready(function() {
 
 //Your jquery goes here
+var player = $("#player");
+
+$(document).keydown(function(e) {
+  $(player).keydown;
+  switch(e.which) {
+
+//move up
+  case 38:
+    $(player).animate({top: "-=40px"}, 'fast');
+    break;
+
+    //move down
+  case 40:
+    $(player).animate({top: "+=40px"}, 'fast');
+    break;
+}; 
+
+});
 
 });

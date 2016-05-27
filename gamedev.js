0c0e01d4e45ff34b677f8abed3b4946ebf326999
KnightDips <210429@hkis.edu.hk>
2016-05-27 10:07:29 +0800

    try to add collision

--- gamedev.js
+++ gamedev.js
@@ -20,9 +20,41 @@ $(document).keydown(function(e) {
 
 });
 
-$("#button").click(function(){
-	$("body").toggle();
-	});
+ function collision($div1, $div2) {
+    var x1 = $div1.offset().left;
+    var y1 = $div1.offset().top;
+    var h1 = $div1.outerHeight(true);
+    var w1 = $div1.outerWidth(true);
+    var b1 = y1 + h1;
+    var r1 = x1 + w1;
+    var x2 = $div2.offset().left;
+    var y2 = $div2.offset().top;
+    var h2 = $div2.outerHeight(true);
+    var w2 = $div2.outerWidth(true);
+    var b2 = y2 + h2;
+    var r2 = x2 + w2;
+    //below is an if statement - if the variables calculate to the right formula, it will return true or false
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+    return true;
+  }
+
+  window.setInterval(function() {
+    //function that makes the magic happen! Below, jQuery prints the word "FALSE" into #results
+   
+    //IMPORTANT!!! Below declares the class of divs that your sprite collides with!!
+    $.each($('.arrow'), function() {
+      if (collision($('#player'), $(this))) { //another if statement. If #myCar DOES hit something, the following will happen:
+        //if #myCar hits .othercar, then #results will say "TRUE"
+        
+
+        //all the actions that happen during a collision go here
+
+      }
+    });
+  }, 200); //this is how often it checks for a collision
+
+}); //document.ready ends - do not delete!!!
 
 
-});
+
+

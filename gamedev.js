7f928b18661c265df3fc18c39f5db15fde0abf56
KnightDips <210429@hkis.edu.hk>
2016-06-02 10:40:39 +0800

    MADE COLLISION WHEN HIT ARROW HIDE

--- gamedev.js
+++ gamedev.js
@@ -42,10 +42,10 @@ var health = 1;
     //function that makes the magic happen! Below, jQuery prints the word "FALSE" into #results
    
     //IMPORTANT!!! Below declares the class of divs that your sprite collides with!!
-    $.each($('.arrow'), function() {
+    $.each($('#arrow'), function() {
       if (collision($('#player'), $(this))) { //another if statement. If #myCar DOES hit something, the following will happen:
         //if #myCar hits .othercar, then #results will say "TRUE"
-        health == 0;
+     $("#player").hide();
         
 
         //all the actions that happen during a collision go here

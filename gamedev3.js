b189a05f1c8fffd7c45ee50478b5b842ed507db3
coolgroundhog <210634@hkis.edu.hk>
2016-06-07 22:54:06 +0800

    t

--- gamedev3.js
+++ gamedev3.js
@@ -15,7 +15,7 @@ var onPlatform = true;
 // This function runs when the page is loaded
 $(document).ready(function(e) {
   // Put the player in the top/middle of the screen
-  $("#player").css("top", "448px").css("left", "387.5px");
+  $("#player").css("top", "15px").css("left", "387.5px");
   // Calculate the player's "ground" position, which is the 
   // height of the stage minus the height of the player
   playerMaxTop = $("#stage3").height() - $("#player").height();
